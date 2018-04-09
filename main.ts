8368066225a9f16a06f68237e28683d769479026
Ludovico O. Russo <ludus.russo@gmail.com>
2018-04-09 15:37:10 +0200

    up

--- main.ts
+++ main.ts
@@ -19,10 +19,8 @@ function createWindow() {
 
   // Create the browser window.
   win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height
+    width: 1280,
+    height: 528
   });
 
   if (serve) {
@@ -37,7 +35,7 @@ function createWindow() {
     }));
   }
 
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Ros, Topic, Message } from 'roslib';
+import * as Leap  from 'leapjs';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  public rad: number = 0;
+  ip: string;
+  connected:boolean = false;
+  ros: Ros;
+  topic: Topic;
+  cmd_topic: Topic;
+  pos: number = 0;
+
+  COMMAND_ALLOWED = ['a', 'd', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+
+  constructor() { }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    console.log('pressed', event.key);
+    console.log(this.cmd_topic);
+    if (this.cmd_topic !== undefined) {
+      console.log('here');
+      if (this.COMMAND_ALLOWED.indexOf(event.key) != -1) {
+        this.cmd_topic.publish(new Message({
+          data: event.key
+        }))
+      }
+    }
+  }
+
+  connect() {
+    this.ros = new Ros({
+      url: 'ws://' + this.ip + ':9090'
+    });
+
+    this.ros.on('connection', () => {
+      this.connected = true;
+      this.topic = new Topic({
+        ros: this.ros,
+        name: '/servo',
+        messageType: 'std_msgs/UInt8'
+      });
+      this.topic.advertise();
+
+      this.cmd_topic = new Topic({
+        ros: this.ros,
+        name: '/web_cmd',
+        messageType: 'std_msgs/String'
+      });
+    });
+
+
+    this.ros.on('error', () => {
+      this.connected = false;
+      console.log('error');
+    });
+
+    this.ros.on('close', () => {
+      this.connected = false;
+    })
+  }
+
+  ngOnInit() {
+
+
+
+    Leap.loop((frame) => {
+      if (frame.hands.length > 0) {
+        let hand = frame.hands[0];
+        this.rad = Math.round(hand.sphereRadius);
+      }
+    });
+
+    setInterval(() => {
+      if (this.topic) {
+        let msg = new Message({
+          data: this.rad
+        });
+        this.topic.publish(msg);  
+      }
+    }, 33);
+
+    console.log(Leap);
+  
+  }
+
+}
