4f33ebcc2e40ab4f555b6ddd4a3dd9072ff6a3ed
Ludovico O. Russo <ludus.russo@gmail.com>
2018-04-17 11:43:13 +0200

    pu

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,66 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { Ros, Topic, Message } from 'roslib';
 import * as Leap  from 'leapjs';
 
+function createAudioMeter(audioContext) {
+	var processor = audioContext.createScriptProcessor(512);
+	processor.onaudioprocess = volumeAudioProcess;
+	processor.clipping = false;
+	processor.lastClip = 0;
+	processor.volume = 0;
+	processor.clipLevel = 0.98;
+	processor.averaging = 0.95;
+	processor.clipLag = 750;
+
+	// this will have no effect, since we don't copy the input to the output,
+	// but works around a current Chrome bug.
+	processor.connect(audioContext.destination);
+
+	processor.checkClipping =
+		function(){
+			if (!this.clipping)
+				return false;
+			if ((this.lastClip + this.clipLag) < window.performance.now())
+				this.clipping = false;
+			return this.clipping;
+		};
+
+	processor.shutdown =
+		function(){
+			this.disconnect();
+			this.onaudioprocess = null;
+		};
+
+	return processor;
+}
+
+function volumeAudioProcess( event ) {
+	var buf = event.inputBuffer.getChannelData(0);
+    var bufLength = buf.length;
+	var sum = 0;
+    var x;
+
+	// Do a root-mean-square on the samples: sum up the squares...
+    for (var i=0; i<bufLength; i++) {
+    	x = buf[i];
+    	if (Math.abs(x)>=this.clipLevel) {
+    		this.clipping = true;
+    		this.lastClip = window.performance.now();
+    	}
+    	sum += x * x;
+    }
+
+    // ... then take the square root of the sum.
+    var rms =  Math.sqrt(sum / bufLength);
+
+    // Now smooth this out with the averaging factor applied
+    // to the previous sample - take the max here because we
+    // want "fast attack, slow release."
+    this.volume = Math.max(rms, this.volume*this.averaging);
+}
+
+
+
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,23 +70,93 @@ import * as Leap  from 'leapjs';
 export class MainComponent implements OnInit {
 
   public rad: number = 0;
-  ip: string;
+  ip: string = "virgil01.local";
   connected:boolean = false;
   ros: Ros;
   topic: Topic;
   cmd_topic: Topic;
   pos: number = 0;
+  audioContext: any;
+  volume: number = 0;
+  led_topic: Topic;
+  volumeOn: boolean = false;
 
-  COMMAND_ALLOWED = ['a', 'd', 'w', 's', 'q', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+  COMMAND_ALLOWED = ['a', 'd', 'w', 's', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
 
-  constructor() { }
+  constructor() { 
+    this.runAudio();
+  }
+
+
+  runAudio() {    
+      // grab an audio context
+      this.audioContext = new AudioContext();
+  
+      // Attempt to get audio input
+      try {  
+          // ask for an audio input
+          navigator.getUserMedia(
+          {
+              "audio": {
+              },
+          }, (stream) => this.gotStream(stream), () => this.didntGetStream() );
+      } catch (e) {
+          alert('getUserMedia threw exception :' + e);
+      }
+  }
+
+  gotStream(stream) {
+    let mediaStreamSource = this.audioContext.createMediaStreamSource(stream);
+    let meter = createAudioMeter(this.audioContext);
+    mediaStreamSource.connect(meter);
+    setInterval( () => this.getVolume(meter), 50);
+  }
+
+  getVolume(meter) {
+    if (this.volumeOn) {
+      this.volume = meter.volume;
+      let cmd = Math.ceil(this.volume*400);
+      if (cmd > 180) cmd = 180;
+      if (cmd < 0) cmd = 0;
+      if (this.topic !== undefined) {
+        this.topic.publish(new Message( {
+          data: cmd
+          }));
+      }
+    }
+  }
+
+  toggleVolume() {
+    this.volumeOn = !this.volumeOn;
+    this.volume = 0;
+    if (this.topic !== undefined) {
+      this.topic.publish(new Message( {
+        data: 1
+        }));
+    }
+  }
+
+  didntGetStream() {
+    console.log('error');
+  }
+
+  setLed(e) {
+    this.setLedValue(+e.target.value)
+  }
+
+  setLedValue(value) {
+    if (this.led_topic != undefined ){
+      this.led_topic.publish( new Message({
+        data: value
+      }));
+    }
+  }
+
+  
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) { 
-    console.log('pressed', event.key);
-    console.log(this.cmd_topic);
     if (this.cmd_topic !== undefined) {
-      console.log('here');
       if (this.COMMAND_ALLOWED.indexOf(event.key) != -1) {
         this.cmd_topic.publish(new Message({
           data: event.key
@@ -42,6 +172,18 @@ export class MainComponent implements OnInit {
 
     this.ros.on('connection', () => {
       this.connected = true;
+      
+      this.topic = new Topic({
+        ros: this.ros,
+        name: '/servo',
+        messageType: 'std_msgs/UInt8'
+      });
+
+      this.led_topic = new Topic({
+        ros: this.ros,
+        name: '/led',
+        messageType: 'std_msgs/UInt16'
+      });
 
 
       this.cmd_topic = new Topic({
